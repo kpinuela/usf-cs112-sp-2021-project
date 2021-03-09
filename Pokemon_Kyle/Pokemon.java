de957db83ffd25c126d7e5f42e979de7526f85aa
kyle <kpinuela@dons.usfca.edu>
2021-03-09 10:59:00 -0800

    commit#

---
+++ Pokemon_Kyle/Pokemon.java
@@ -0,0 +1,43 @@
+package parentclassexercise;
+
+public class Pokemon {
+	public int weight;
+	public String type;
+	
+	public Pokemon(){ //default constructor
+		this.weight = 0;
+		this.type = "";
+		
+	}
+	
+	
+	public Pokemon(int wParameter, String tParameter) {
+		this.weight = wParameter;
+		this.type = tParameter;
+		
+		
+	}
+	public void canFitInPokeball() {
+		System.out.println("Can Fit in Pokeball");
+	
+	}
+	public void getWeight() {
+		System.out.println(weight);
+	}
+	
+	public void getType() {
+		System.out.println(type);
+	}
+	public void setWeight(int wParameter) {
+		this.weight = wParameter;
+	}
+	public void setType(String tParameter) {
+		this.type = tParameter;
+	}
+	public void move() {
+		System.out.println("cam move.");
+		
+	}
+}
+
+
